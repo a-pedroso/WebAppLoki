cb024623bf52242a0f394991f1f6b0e54e862716
Andre Pedroso <andre.pedroso@ceiia.com>
2020-09-25 13:57:02 +0100

    web app and docker compose

---
+++ src/WebAppLoki/Controllers/LogController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppLoki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Debug")]
+        public string Debug()
+        {
+            _logger.LogDebug($"{DateTime.UtcNow:yyyy-MM-dd}");
+            return "Debug log";
+        }
+
+        [HttpGet("Info")]
+        public string Info()
+        {
+            _logger.LogInformation($"{DateTime.UtcNow:yyyy-MM-dd}");
+            return "Information log";
+        }
+
+        [HttpGet("Warn")]
+        public string Warn()
+        {
+            _logger.LogWarning($"{DateTime.UtcNow:yyyy-MM-dd}");
+            return "Warning log";
+        }
+
+        [HttpGet("Error")]
+        public string Error()
+        {
+            _logger.LogError($"{DateTime.UtcNow:yyyy-MM-dd}");
+            return "Error log";
+        }
+
+        [HttpGet("Critical")]
+        public string Critical()
+        {
+            _logger.LogCritical($"{DateTime.UtcNow:yyyy-MM-dd}");
+            return "Critical log";
+        }
+    }
+}
---
+++ src/WebAppLoki/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Loki;
+using System;
+using System.IO;
+
+namespace WebAppLoki
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                //.UseSerilog((builder, cfg) =>
+                //{
+                //    cfg.ReadFrom.Configuration(builder.Configuration);
+                //})
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information($"WebAppLoki started at {DateTime.UtcNow}");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
